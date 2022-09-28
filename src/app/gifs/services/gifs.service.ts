3a07f4b259ac71e6b6acea2329b3ddeacb93bbcc
Ezequiel <zapata.ed1989@gmail.com>
2022-09-28 15:15:18 -0300

    fix endpoint params

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 export class GifsService {
 
   private apiKey: string = 'UXyeFEuXasKK02Oy7RLaKXYV1wpFE7R7';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
   public resultados: Gif[] = []
 
@@ -41,7 +42,12 @@ export class GifsService {
       localStorage.setItem('historial', JSON.stringify( this._historial ))
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
         .subscribe( ( resp )=> {
           console.log(resp.data)
           this.resultados = resp.data
